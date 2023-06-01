1b7e561373ce3dbc7ef92ead12d27287b6bd9b24
Grant Billings <gtbillin@ncsu.edu>
2023-06-01 16:45:21 -0400

    moved FMindex out of bwt

--- src/bwt.jl
+++ src/bwt.jl
@@ -11,27 +11,7 @@ struct Rank
     tots::Dict{UInt8, UInt64}
 end
 
-struct FMIndex
-    # F is the the number of times each character occurs
-    # in the original string
-    F::Dict{UInt8, UInt64}
 
-    # L is the Burrows-Wheeler transform of the original string
-    L::Vector{UInt8}
-
-    # SA is the suffix array, corresponding to L
-    SA::Vector{UInt64}
-
-    # T is the tally on the number of times each character occurs
-    # along L
-    # The amount of space can be reduced substantially by messing with this term
-    T::Dict{UInt8, Vector{UInt64}}
-end
-
-# get the alphabet for an FMIndex
-function get_σ(fm::FMIndex)
-    return sort(collect(keys(fm.F)))
-end
 
 # now we want to create a writer for FM index.
 # ideally this will have four components:
@@ -195,14 +175,4 @@ function tallyViaBwt(bw::Vector{UInt8})
     tally = Dict{UInt8, Vector{UInt64}}(σ[i] => bt[i] for i in eachindex(σ))
 
     return tally
-end
-
-function Base.display(fm::FMIndex)
-    σ = get_σ(fm)
-    display(String(Char.(σ[2:end])))
-    display(String(Char.(fm.L)))
-    println(Int64.(fm.SA))
-    for ch in σ
-        println(Char.(ch), " ", Int64.(fm.T[ch]))
-    end
 end
--- src/fm.jl
+++ src/fm.jl
@@ -2,6 +2,40 @@
 
 # convert all of the below code from python to julia
 
+struct FMIndex
+    # F is the the number of times each character occurs
+    # in the original string
+    F::Dict{UInt8, UInt64}
+
+    # L is the Burrows-Wheeler transform of the original string
+    L::Vector{UInt8}
+
+    # SA is the suffix array, corresponding to L
+    SA::Vector{UInt64}
+
+    # T is the tally on the number of times each character occurs
+    # along L
+    # The amount of space can be reduced substantially by messing with this term
+    T::Dict{UInt8, Vector{UInt64}}
+end
+
+# get the alphabet for an FMIndex
+function get_σ(fm::FMIndex)
+    return sort(collect(keys(fm.F)))
+end
+
+# pretty print an FMIndex
+function Base.display(fm::FMIndex)
+    σ = get_σ(fm)
+    display(String(Char.(σ[2:end])))
+    display(String(Char.(fm.L)))
+    println(Int64.(fm.SA))
+    for ch in σ
+        println(Char.(ch), " ", Int64.(fm.T[ch]))
+    end
+end
+
+
 struct FMCheckpoints
     0
 end
