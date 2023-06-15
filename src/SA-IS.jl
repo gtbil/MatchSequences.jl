84f694c06a80c451d618160dd028339e79499cda
Grant Billings <gtbillin@ncsu.edu>
2023-06-15 14:43:01 -0400

    finally got the initial sort working

--- src/SA-IS.jl
+++ src/SA-IS.jl
@@ -1,5 +1,4 @@
 # import LoopVectorization
-
 # global const σ = UInt8.(collect("\$ACGT"))
 global const σ = UInt8.(collect("abcdefg"))
 global const σ_to_Col = Dict{UInt8, Int8}(ch => i for (i, ch) in enumerate(σ))
@@ -18,34 +17,37 @@ A non-modifying version of `charBeforeIsSame!`.
 """
 function charBeforeIsSame(t::Vector{UInt8})::BitVector
     # create an un-initialized bitvector
-    out_vec = BitArray{1}(undef, length(t) + 1)
-    return charBeforeIsSame!(t, out_vec)
+    issame = BitArray{1}(undef, length(t) + 1)
+
+    charBeforeIsSame!(t, issame)
+
+    return issame
 end
 
 # this version should be free of allocations
 
 """
-    charBeforeIsSame!(t, out_vec)
+    charBeforeIsSame!(t, issame)
 
 Helper function. Returns a BitVector that is `length(t) + 1` long.
 
 # Arguments:
 * `t::Vector{UInt8}`  : Vector of UInt8 characters.
-* `out_vec::BitVector`: BitVector to store the output in (`undef` is OK)
+* `issame::BitVector`: BitVector to store the output in (`undef` is OK)
 """
-function charBeforeIsSame!(t::Vector{UInt8}, out_vec::BitVector)::BitVector
+function charBeforeIsSame!(t::Vector{UInt8}, issame::BitVector)::BitVector
 
     # set the first value to be false (it cannot be equal to the substring 
     # before it)
-    out_vec[1:1] .= 0
+    issame[1:1] .= 0
 
     # use this weird slicing syntax to avoid any allocations
-    @views out_vec[2:length(t)] .= t[2:length(t)] .== t[1:(length(t)-1)]
+    @views issame[2:length(t)] .= t[2:length(t)] .== t[1:(length(t)-1)]
 
     # set the empty string to always be not equal to char before it
-    out_vec[end:end] .= 0
+    issame[end:end] .= 0
 
-    return out_vec
+    return issame
 end
 
 # find the indices of L-Type suffixes
@@ -56,87 +58,93 @@ A non-modifying version of `suffixIsLType!`.
 """
 function suffixIsLType(t::Vector{UInt8})::BitVector
     # get the sameVec
-    same_vec = BitArray{1}(undef, length(t) + 1)
-    out_vec = BitArray{1}(undef, length(t) + 1)
+    issame = BitArray{1}(undef, length(t) + 1)
+    isltype = BitArray{1}(undef, length(t) + 1)
 
-    return suffixIsLType!(t, same_vec, out_vec)
+    suffixIsLType!(t, issame, isltype)
+
+    return isltype
 end
 
 # find the indices of L-Type suffixes
 # L-type suffixes are LARGER than the suffix to the right
 
 """
-    suffixIsLType!(t, same_vec, out_vec)
+    suffixIsLType!(t, issame, isltype)
 
 Returns a BitVector that is `length(t) + 1` long.
 
 # Arguments:
 * `t::Vector{UInt8}`   : Vector of UInt8 characters.
-* `same_vec::BitVector`: A BitVector of `length(t) + 1`.
+* `issame::BitVector`  : A BitVector of `length(t) + 1`.
                          It will end up indicating if each character is equal 
                             to the one immediately before it. 
                          `suffixIsLType!` will be called internally.
-* `out_vec::BitVector` : BitVector to store the output in (`undef` is OK). 
+* `isltype::BitVector` : BitVector to store the output in (`undef` is OK). 
                          Will be modified in place, returned for convenience.
 """
 function suffixIsLType!(t::Vector{UInt8}, 
-                        same_vec::BitVector,
-                        out_vec::BitVector)::BitVector
-    same_vec = charBeforeIsSame!(t, same_vec)
+                        issame::BitVector,
+                        isltype::BitVector)::BitVector
+    charBeforeIsSame!(t, issame)
 
     # we know a character must be L type if it is larger than the character
     # immediately to its right
     # so we can reliably mark these as L type substrings
-    @views out_vec[1:(length(t)-1)] .= t[1:(length(t)-1)] .> t[2:(length(t))]
+    @views isltype[1:(length(t)-1)] .= t[1:(length(t)-1)] .> t[2:(length(t))]
     
     # the last character before the empty character must be L type
-    out_vec[length(t):length(t)] .= 1
+    isltype[length(t):length(t)] .= 1
 
     # the empty character is guaranteed to be R type
-    out_vec[end:end] .= 0
+    isltype[end:end] .= 0
 
     # now move our way up same_vec, and perform the same ops as we go
     for i in length(t):-1:1
-        @views out_vec[i:i] .= out_vec[i] || (same_vec[i+1] && (out_vec[(i+1)]))
+        @views isltype[i:i] .= isltype[i] || (issame[i+1] && (isltype[(i+1)]))
     end
 
-    return out_vec
+    return isltype
 end
 
 """
 A non-modifying version of `suffixIsLMS!`.
 """
-function suffixIsLMS(l_vec::BitVector)::BitVector
-    out_vec = BitArray{1}(undef, length(l_vec))
+function suffixIsLMS(isltype::BitVector)::BitVector
+    islms = BitArray{1}(undef, length(isltype))
 
-    out_vec = suffixIsLMS!(l_vec, out_vec)
+    suffixIsLMS!(isltype, islms)
 
-    return out_vec
+    return islms
 end
 
 """
+    suffixIsLMS!(isltype, islms)
+
 Given a BitVector indicating if each suffix is a left-most S-type or not.
+
 # Arguments:
-* `l_vec::BitVector`   : BitVector indicating if each suffix is L-type or not.
-* `out_vec::BitVector` : BitVector to store the output in.
+* `isltype::BitVector`   : BitVector indicating if each suffix is L-type or not.
+* `islms::BitVector` : BitVector to store the output in.
 """
-function suffixIsLMS!(l_vec::BitVector,
-                      out_vec::BitVector)::BitVector
+function suffixIsLMS!(isltype::BitVector,
+                      islms::BitVector)::BitVector
 
     # the first suffix will never be a left-most S-type
-    out_vec[1:1] .= 0
+    islms[1:1] .= 0
 
     # a suffix is a left-most S-type if it is an S-type and the character before
     # it is an L-type
     # the syntax below says we will set the output vector to true if the 
     # previous character is L-type (true in the first part before `&.`,
     # and the current character is S-type (broadcasted not with .~).
-    @views out_vec[2:end] .= l_vec[1:(end-1)] .& .~ l_vec[2:(end)]
-    return out_vec
+    @views islms[2:end] .= isltype[1:(end-1)] .& .~ isltype[2:(end)]
+    
+    return islms
 end
 
 """
-    suffixIsLMSEqual(t, lms_vec, offsetA, offsetB)
+    suffixIsLMSEqual(t, lms_vec, offset_a, offset_b)
 
 Given the input sequence and a vector indiciating which characters are L-type,
 as well as offsets into each vector, will check to see if two LMS substrings are
@@ -146,31 +154,31 @@ Allocates two ints.
 
 # Arguments:
 * `t::Vector{UInt8}`   : Vector of UInt8 characters.
-* `lms_vec::BitVector` : BitVector indicating if each suffix is a left-most 
+* `islms::BitVector`   : BitVector indicating if each suffix is a left-most 
                            S-type or not.
-* `offsetA::UInt64`    : Offset into `s` for the first LMS substring.
-* `offsetB::UInt64`    : Offset into `s` for the second LMS substring.
+* `offset_a::UInt64`   : Offset into `s` for the first LMS substring.
+* `offset_b::UInt64`   : Offset into `s` for the second LMS substring.
 """
 function suffixIsLMSEqual(t::Vector{UInt8},
-                          lms_vec::BitVector,
-                          offsetA::UInt64,
-                          offsetB::UInt64)::Bool
+                          islms::BitVector,
+                          offset_a::UInt64,
+                          offset_b::UInt64)::Bool
     # return false if the one offset equals the end of the string
-    if (offsetA == length(t) + 1) | (offsetB == length(t) + 1)
+    if (offset_a == length(t) + 1) | (offset_b == length(t) + 1)
         return false
     end
 
     # find the next left-most s-type for each offset
-    nextA = findnext(lms_vec, offsetA + 1) - 1
-    nextB = findnext(lms_vec, offsetB + 1) - 1
+    next_a = findnext(islms, offset_a + 1) - 1
+    next_b = findnext(islms, offset_b + 1) - 1
 
     # return if they are not the same length
-    if (offsetA - nextA) != (offsetB - nextB)
+    if (offset_a - next_a) != (offset_b - next_b)
         return false
     end
     
     # try to see if we can do the check without allocations
-    return @views allequal(t[offsetA:nextA] .== t[offsetB:nextB])
+    return @views allequal(t[offset_a:next_a] .== t[offset_b:next_b])
 end
 
 # make the buckets
@@ -180,106 +188,138 @@ end
 A non-modifying version of `findBucketSizes!`.
 """
 function findBucketSizes(t::Vector{UInt8}; alphabet=σ)::Vector{UInt64}
-    out_vec = Vector{UInt64}(undef, length(alphabet))
+    bucketsizes = Vector{UInt64}(undef, length(alphabet))
 
-    out_vec = findBucketSizes!(t, out_vec; alphabet=alphabet)
+    findBucketSizes!(t, bucketsizes; alphabet=alphabet)
 
-    return out_vec
+    return bucketsizes
 end
 
 """
-    findBucketSizes!(t, out_vec; alphabet=σ)
+    findBucketSizes!(t, bucketsizes; alphabet=σ)
 
-Given the string, `t`, and two temporary vectors, `temp_vec` and `out_vec`, as
+Given the string, `t`, and one temporary vector, `bucketsizes`, as
 well as an optional `alphabet` kwar, will find the size of each bucket. The
 primary operation is to computer a temporary bit-mask and immediately consume
 it, using `count!`
 # Arguments:
-* `t::Vector{UInt8}`   : Vector of UInt8 characters.
-* `out_vec::Vector{UInt64}` : Vector to store the output in.
-* `alphabet::Vector{UInt8}` : Vector of UInt8 characters to use as the alphabet.
+* `t::Vector{UInt8}`            : Vector of UInt8 characters.
+* `bucketsizes::Vector{UInt64}` : Vector to store the output in.
+* `alphabet::Vector{UInt8}`     : Vector of UInt8 characters to use as the 
+                                  alphabet.
 """
 function findBucketSizes!(t::Vector{UInt8}, 
-                          out_vec::Vector{UInt64}; alphabet=σ)::Vector{UInt64}
+                          bucketsizes::Vector{UInt64}; 
+                          alphabet=σ)::Vector{UInt64}
 
     # make a bitmask for each character, and then sum it
     # this way, the bitmask is consumed as it is produced
     
     for ch in alphabet
         @views count!(==(ch), 
-                      out_vec[σ_to_Col[ch]:σ_to_Col[ch]],
+                      bucketsizes[σ_to_Col[ch]:σ_to_Col[ch]],
                       t)
     end
 
-    return out_vec
+    return bucketsizes
 end
 
 """
 A non-modifying version of `findBucketHeads`.
 """
-function findBucketHeads(bucket_sz::Vector{UInt64})::Vector{UInt64}
-    out_vec = Vector{UInt64}(undef, length(bucket_sz))
-    out_vec = findBucketHeads!(bucket_sz, out_vec)
-    return out_vec
+function findBucketHeads(bucketsizes::Vector{UInt64})::Vector{UInt64}
+    heads = Vector{UInt64}(undef, length(bucketsizes))
+    
+    findBucketHeads!(bucketsizes, heads)
+
+    return heads
 end
 
 """
-    findBucketHeads!(bucket_sz, out_vec)
+    findBucketHeads!(bucketsizes, heads)
 
 A function that finds where a group of characters will _start_ in the resulting
 suffix array. Note position `1` is taken up by the empty string, so this will
 need to be trimmed out by the caller.
 # Arguments:
-* `bucket_sz::Vector{UInt64}` : Vector of UInt64 indicating the size of each
+* `bucketsizes::Vector{UInt64}` : Vector of UInt64 indicating the size of each
                                 bucket.
-* `out_vec::Vector{UInt64}``  : Vector to store the output in.
+* `heads::Vector{UInt64}``      : Vector to store the output in.
 """
-function findBucketHeads!(bucket_sz::Vector{UInt64},
-                          out_vec::Vector{UInt64})::Vector{UInt64}
+function findBucketHeads!(bucketsizes::Vector{UInt64},
+                          heads::Vector{UInt64})::Vector{UInt64}
     # set the first value to be 0 (will be dealt with later)
-    out_vec[1:1] .= 0
+    heads[1:1] .= 0
 
     # replace all the following values with the cumulative sum
     # indicating how many characters are in the string sorted up through
     # this point
-    @views cumsum!(out_vec[2:end], bucket_sz[1:(end-1)])
+    @views cumsum!(heads[2:end], bucketsizes[1:(end-1)])
 
     # add two to each, since place [1] will always be the empty string
-    out_vec .+= 2
+    heads .+= 2
 
-    return out_vec
+    return heads
 end
 
 """
 A non-modifying version of `findBucketTails`.
 """
-function findBucketTails(bucket_sz::Vector{UInt64})::Vector{UInt64}
-    out_vec = Vector{UInt64}(undef, length(bucket_sz))
-    out_vec = findBucketTails!(bucket_sz, out_vec)
-    return out_vec
+function findBucketTails(bucketsizes::Vector{UInt64})::Vector{UInt64}
+    tails = Vector{UInt64}(undef, length(bucketsizes))
+    
+    findBucketTails!(bucketsizes, tails)
+    
+    return tails
 end
 
 """
-    findBucketTails!(bucket_sz, out_vec)
+    findBucketTails!(bucketsizes, tails)
 
 A function that finds where a group of characters will _end_ in the resulting
 suffix array. Note position `1` is taken up by the empty string, so this will
 need to be trimmed out by the caller.
 # Arguments:
-* `bucket_sz::Vector{UInt64}` : Vector of UInt64 indicating the size of each
+* `bucketsizes::Vector{UInt64}` : Vector of UInt64 indicating the size of each
                                 bucket.
-* `out_vec::Vector{UInt64}``  : Vector to store the output in.
+* `tails::Vector{UInt64}``      : Vector to store the output in.
 """
-function findBucketTails!(bucket_sz::Vector{UInt64},
-                          out_vec::Vector{UInt64})::Vector{UInt64}
+function findBucketTails!(bucketsizes::Vector{UInt64},
+                          tails::Vector{UInt64})::Vector{UInt64}
     # count the number of characters seen up until x point
-    @views cumsum!(out_vec, bucket_sz)
+    @views cumsum!(tails, bucketsizes)
 
     # add one to each, since place [1] will always be the empty string
     # and we need to account for this
-    out_vec .+= 1
+    tails .+= 1
 
-    return out_vec
+    return tails
+end
+
+"""
+    guessLMSSort!(t, sa, isltype, islms, tails)
+
+Using a generator expression? 
+
+https://discourse.julialang.org/t/iterator-for-indexes-of-true-values-in-bitvector-without-allocations/100411/5
+"""
+function guessLMSSort!(t::Vector{UInt8}, 
+                       sa::Vector{UInt64},
+                       islms::BitVector, 
+                       tails::Vector{UInt64})::Vector{UInt64}
+
+    # set the empty string to be the first suffix
+    sa[1:1] .= length(t) + 1
+
+    # iterate along the true values in islms (no allocations?)
+    # until we reach the empty string
+    # solution from here
+    @views for idx in Iterators.map(first, Iterators.filter(last, pairs(islms[1:(end-1)])))
+        sa[tails[σ_to_Col[t[idx]]]:tails[σ_to_Col[t[idx]]]] .= idx
+        tails[σ_to_Col[t[idx]]:σ_to_Col[t[idx]]] .-= 1
+    end
+                       
+    return sa
 end
 
 """
@@ -287,23 +327,35 @@ A benchmarking function that will eventually be used to call the entire
 suffix sorting routine.
 """
 function benchmark(t::Vector{UInt8}, 
-                   l_vec::BitVector, lms_vec::BitVector,
-                   out_vec::Vector{UInt64}, 
+                   sa::Vector{UInt64},
+                   isltype::BitVector, 
+                   islms::BitVector,
+                   bucketsizes::Vector{UInt64}, 
                    heads::Vector{UInt64}, 
                    tails::Vector{UInt64};
                    σ_in::Vector{UInt8})
     
     # build the type map
-    MatchSequences.suffixIsLType!( t , lms_vec, l_vec )
+    # islms is used as a temporary variable here
+    # for tracking if adjacent symbols are equal
+    MatchSequences.suffixIsLType!( t , islms, isltype )
 
     # determine which suffixes are LMS
-    MatchSequences.suffixIsLMS!( l_vec, lms_vec )
+    # use the type vector to find the left-most S-type suffixes
+    MatchSequences.suffixIsLMS!( isltype, islms )
 
-    MatchSequences.findBucketSizes!(t, out_vec; alphabet = σ_in)
-    MatchSequences.findBucketHeads!(out_vec2, heads)
-    MatchSequences.findBucketTails!(out_vec2, tails)
+    # get the sizes of each bucket
+    MatchSequences.findBucketSizes!(t, bucketsizes; alphabet = σ_in)
 
-    return l_vec, lms_vec, heads, tails
+    # then find their heads and tails
+    MatchSequences.findBucketHeads!(bucketsizes, heads)
+    MatchSequences.findBucketTails!(bucketsizes, tails)
+
+    # insert all the LMS suffixes into approximately the right place
+    # in the suffix array
+    MatchSequences.guessLMSSort!(t, sa, islms, tails)
+
+    return isltype, islms, heads, tails, sa
 end
 
 #=
@@ -327,10 +379,16 @@ out_vec = Vector{UInt64}(undef, length(alpha))
 heads = Vector{UInt64}(undef, length(alpha))
 tails = Vector{UInt64}(undef, length(alpha))
 
-out = MatchSequences.benchmark(t, l_vec, lms_vec, out_vec, heads, tails; σ_in = alpha);
+# allocate a vector for the suffix array
+sa = Vector{UInt64}(undef, length(t) + 1)
+# use zeroes for now
+sa = zeros(UInt64, length(t) + 1)
+
+out = MatchSequences.benchmark(t, sa, l_vec, lms_vec, out_vec, heads, tails; σ_in = alpha);
+
 map(x -> Int.(x), out)
 
-BenchmarkTools.@btime MatchSequences.benchmark($t, $l_vec, $lms_vec, $out_vec, $heads, $tails; σ_in = $alpha);
+BenchmarkTools.@btime MatchSequences.benchmark($t, $sa, $l_vec, $lms_vec, $out_vec, $heads, $tails; σ_in = $alpha);
 
 100_000  222.304 ms (0 allocations: 0 bytes)
 
